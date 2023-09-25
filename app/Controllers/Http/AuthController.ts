8b8008b280bdbcad7bf0a615e0ac583860723ab4
Elias Yanez <gtayanez@gmail.com>
2023-09-25 11:13:26 -0300

    add controllers

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,32 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+export default class AuthController {
+  public async login({ request, auth }: HttpContextContract) {
+    const email = await request.input('email')
+    const password = request.input('password')
+
+    const token = await auth.use('api').attempt(email, password, {
+      expiresIn: '10 days',
+    })
+    return token.toJSON()
+  }
+
+  public async register({ request, auth }: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
+    const name = request.input('name')
+
+    //creacion de nuevo usuario
+    const user = new User()
+    user.email = email
+    user.name = name
+    user.password = password
+    await user.save()
+
+    const token = await auth.use('api').login(user, {
+      expiresIn: '10 days',
+    })
+    return token.toJSON()
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,15 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.group(() => {
+  Route.post('register', 'AuthController.register')
+  Route.post('login', 'AuthController.login')
+
+  Route.group(() => {
+    Route.get('todos', 'TodosController.index')
+    Route.get('todos/:id', 'TodosController.show')
+    Route.put('todos/update', 'TodosController.update')
+    Route.post('todos', 'TodosController.store')
+  }).middleware('auth:api')
+}).prefix('api')
